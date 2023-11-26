3ff0cfa0be91e00124b9fe13befec479cb670b43
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-11-26 22:18:43 +0300

    c

---
+++ compile.js
@@ -0,0 +1,3 @@
+//we can not import the contract as require(...path) because javascript will consider it as a JS code,however it is solidity code
+//so we need to access the smart contract from hard disk
+
