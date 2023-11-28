3eaca910ac16b4048c527ca0c90ad20629f547c4
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-11-28 14:47:49 +0300

    initial

--- compile.js
+++ compile.js
@@ -1,3 +1,31 @@
 //we can not import the contract as require(...path) because javascript will consider it as a JS code,however it is solidity code
 //so we need to access the smart contract from hard disk
 
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname,'contracts','Inbox.sol') //dirname : all directory until this file location
+const source = fs.readFileSync(inboxPath,'utf8')
+
+const input = {
+    language: 'Solidity',
+    sources: {
+      'Inbox.sol': {
+        content: source,
+      },
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*'],
+        },
+      },
+    },
+  };
+
+  module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+    'Inbox.sol'
+  ].Inbox;
+
+  console.log(JSON.parse(solc.compile(JSON.stringify(input))).contracts);
---
+++ test/inbox.test.js
@@ -0,0 +1,3 @@
+const assert = require('assert') //testler hakkinda iddialarda bulunuruz
+const ganache = require('ganache')
+const {Web3} = require('web3') //constructor of web3 instances, and also it is starting with capitalized letter since it is a constructor function(like a class)
