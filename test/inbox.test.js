c3640a65b341d6e72115079b6fd1a68216baab8d
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-11-28 16:20:47 +0300

    test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,28 @@
 const assert = require('assert') //testler hakkinda iddialarda bulunuruz
 const ganache = require('ganache')
 const {Web3} = require('web3') //constructor of web3 instances, and also it is starting with capitalized letter since it is a constructor function(like a class)
+
+const web3 = new Web3(ganache.provider())
+
+class Car {
+    park() {
+        return 'stopped'
+    }
+    drive() {
+        return 'vroom'
+    }
+}
+let car;
+beforeEach(() => {
+    car = new Car()
+})
+
+describe('Car', () => {
+    it('park should return a string',() => {
+        assert.equal(car.park(),'stopped')
+    });
+
+    it('can drive and return vroom',() => {
+        assert.equal(car.drive(),'vroom')
+    })
+})
