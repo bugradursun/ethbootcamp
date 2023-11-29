77032e09e1703af7fc8bdd4645ce652a59a33f16
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-11-29 09:57:00 +0300

    compile

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,28 +1,21 @@
 const assert = require('assert') //testler hakkinda iddialarda bulunuruz
 const ganache = require('ganache')
 const {Web3} = require('web3') //constructor of web3 instances, and also it is starting with capitalized letter since it is a constructor function(like a class)
-
 const web3 = new Web3(ganache.provider())
+const {interface,bytecode} = require('../compile')
 
-class Car {
-    park() {
-        return 'stopped'
-    }
-    drive() {
-        return 'vroom'
-    }
-}
-let car;
-beforeEach(() => {
-    car = new Car()
+let accounts;
+
+beforeEach(async () => {
+    //get a list of all accounts
+    accounts = await web3.eth.getAccounts()
+
+    //use one of those accounts to deploy the contract
+    new web3.eth.Contract(JSON.parse(interface)).deploy({data:bytecode,arguments:['Hi there!']})
 })
 
-describe('Car', () => {
-    it('park should return a string',() => {
-        assert.equal(car.park(),'stopped')
-    });
-
-    it('can drive and return vroom',() => {
-        assert.equal(car.drive(),'vroom')
+describe('Inbox',() => {
+    it('deploys a contract',() => {
+        console.log(accounts)
     })
 })
