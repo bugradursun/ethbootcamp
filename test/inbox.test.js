bbece6f4d61f27a1b60d5a3709ab864997133c51
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-11-29 12:16:59 +0300

    test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,20 +2,27 @@ const assert = require('assert') //testler hakkinda iddialarda bulunuruz
 const ganache = require('ganache')
 const {Web3} = require('web3') //constructor of web3 instances, and also it is starting with capitalized letter since it is a constructor function(like a class)
 const web3 = new Web3(ganache.provider())
-const {interface,bytecode} = require('../compile')
+
+const {abi,evm} = require('../compile')
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     //get a list of all accounts
     accounts = await web3.eth.getAccounts()
 
     //use one of those accounts to deploy the contract
-    new web3.eth.Contract(JSON.parse(interface)).deploy({data:bytecode,arguments:['Hi there!']})
+    inbox = await new web3.eth.Contract(abi).deploy({ //deploy just states we initialize the smart contract,not has sent to web3 yet
+        data:evm.bytecode.object, //evm compile.js'deki bytecode
+        arguments:['Hi there!'],
+    })
+    .send({from:accounts[0],gas:'1000000'}) //send method actually sends the smart contract to web3. 
 })
 
 describe('Inbox',() => {
     it('deploys a contract',() => {
-        console.log(accounts)
+        assert.ok(inbox.options.address) //inbox abi'da options objesinin altinda address methodu var,assert.ok ile bunu kontrol ediyoruz, ok mthodu is this defined value kontrol eder
+
     })
 })
